17a8cf8ae5a1a6b35624d1ce951334a7dfe8b4ac
Melissa Carbone <mcarbone1@gmail.com>
2014-01-05 11:20:51 -0500

    Restrict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 PropertyManagement::Application.routes.draw do
 
-  resources :buildings
+  resources :buildings, only: [:new, :create]
 
-  resources :owners
+  resources :owners, except: [:show, :update, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
